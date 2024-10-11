707fd386a5375b2c9e4f8faa1760aa614c10895c
wowdrone <cumonyo@naver.com>
2024-10-11 17:59:25 +0900

    Fix Video-#11421

--- src/main.cc
+++ src/main.cc
@@ -7,35 +7,61 @@
  *
  ****************************************************************************/
 
-#include <QtCore/QProcessEnvironment>
-#include <QtCore/QtPlugin>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QMessageBox>
-#include <QtQuick/QQuickWindow>
-
 #include "QGCApplication.h"
 #include "QGC.h"
 #include "AppMessages.h"
-
+#include "QGCMapEngine.h"
+#include "Vehicle.h"
 #ifndef __mobile__
-    #include "RunGuard.h"
+#ifndef NO_SERIAL_LINK
+#include <QtSerialPort/QSerialPort>
+#endif
+#include "QGCSerialPortInfo.h"
+#include "RunGuard.h"
 #endif
 
 #ifdef Q_OS_ANDROID
-    #include "AndroidInterface.h"
+#include "AndroidInterface.h"
+#ifndef NO_SERIAL_LINK
+#include "qserialport.h"
+#endif
+#endif
+
+#ifdef UNITTEST_BUILD
+#include "UnitTest.h"
 #endif
 
 #ifdef QT_DEBUG
-
 #include "CmdLineOptParser.h"
+#ifdef Q_OS_WIN
+#include <crtdbg.h>
+#endif
+#endif
 
-#ifdef UNITTEST_BUILD
-#include "UnitTestList.h"
+#include <QtWidgets/QApplication>
+#include <QtGui/QIcon>
+#include <QtWidgets/QMessageBox>
+#include <QtCore/QProcessEnvironment>
+#include <QtCore/QtPlugin>
+#include <QtQuickControls2/QQuickStyle>
+#include <QtQuick/QQuickWindow>
+#ifdef QGC_ENABLE_BLUETOOTH
+#include <QtBluetooth/QBluetoothSocket>
+#endif
+
+ /* SDL does ugly things to main() */
+#ifdef main
+#undef main
+#endif
+
+#ifndef __mobile__
+#ifndef NO_SERIAL_LINK
+Q_DECLARE_METATYPE(QGCSerialPortInfo)
+#endif
 #endif
 
 #ifdef Q_OS_WIN
 
-#include <crtdbg.h>
 #include <windows.h>
 #include <iostream>
 
@@ -50,23 +76,18 @@ int WindowsCrtReportHook(int reportType, char* message, int* returnValue)
     return true;                        // We handled this fully ourselves
 }
 
-#endif // Q_OS_WIN
-
-#endif // QT_DEBUG
+#endif
 
 // To shut down QGC on Ctrl+C on Linux
 #ifdef Q_OS_LINUX
-
 #include <csignal>
 
 void sigHandler(int s)
 {
     std::signal(s, SIG_DFL);
-    if(qgcApp()) {
-        qgcApp()->mainRootWindow()->close();
-        QEvent event{QEvent::Quit};
-        qgcApp()->event(&event);
-    }
+    qgcApp()->mainRootWindow()->close();
+    QEvent event{ QEvent::Quit };
+    qgcApp()->event(&event);
 }
 
 #endif /* Q_OS_LINUX */
@@ -80,38 +101,25 @@ void sigHandler(int s)
  * @return exit code, 0 for normal exit and !=0 for error cases
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 #ifndef __mobile__
     // We make the runguard key different for custom and non custom
     // builds, so they can be executed together in the same device.
-    // Stable and Daily have same QGC_APP_NAME so they would
+    // Stable and Daily have same QGC_APPLICATION_NAME so they would
     // not be able to run at the same time
-    const QString runguardString = QString("%1 RunGuardKey").arg(QGC_APP_NAME);
+    QString runguardString(QGC_APPLICATION_NAME);
+    runguardString.append("RunGuardKey");
 
     RunGuard guard(runguardString);
     if (!guard.tryToRun()) {
         // QApplication is necessary to use QMessageBox
         QApplication errorApp(argc, argv);
         QMessageBox::critical(nullptr, QObject::tr("Error"),
-            QObject::tr("A second instance of %1 is already running. Please close the other instance and try again.").arg(QGC_APP_NAME)
+            QObject::tr("A second instance of %1 is already running. Please close the other instance and try again.").arg(QGC_APPLICATION_NAME)
         );
         return -1;
     }
-#endif
-
-#ifdef Q_OS_LINUX
-#ifndef Q_OS_ANDROID
-    if (getuid() == 0) {
-        QApplication errorApp(argc, argv);
-        QMessageBox::critical(nullptr, QObject::tr("Error"),
-            QObject::tr("You are running %1 as root. "
-                "You should not do this since it will cause other issues with %1."
-                "%1 will now exit.<br/><br/>").arg(QGC_APP_NAME)
-        );
-        return -1;
-    }
-#endif
 #endif
 
     //-- Record boot time
@@ -119,9 +127,8 @@ int main(int argc, char *argv[])
 
 #ifdef Q_OS_UNIX
     //Force writing to the console on UNIX/BSD devices
-    if (!qEnvironmentVariableIsSet("QT_LOGGING_TO_CONSOLE")) {
+    if (!qEnvironmentVariableIsSet("QT_LOGGING_TO_CONSOLE"))
         qputenv("QT_LOGGING_TO_CONSOLE", "1");
-    }
 #endif
 
     // install the message handler
@@ -131,13 +138,17 @@ int main(int argc, char *argv[])
 #ifndef Q_OS_IOS
     // Prevent Apple's app nap from screwing us over
     // tip: the domain can be cross-checked on the command line with <defaults domains>
-    QProcess::execute("defaults", {"write org.qgroundcontrol.qgroundcontrol NSAppSleepDisabled -bool YES"});
+    QProcess::execute("defaults", { "write org.qgroundcontrol.qgroundcontrol NSAppSleepDisabled -bool YES" });
 #endif
 #endif
 
 #ifdef Q_OS_WIN
+    // Unlike Qt5, Qt6 on Windows use Direct3D by default,
+    // so force it to use OpenGL since it is required by qt6glitem
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+
     // Set our own OpenGL buglist
-    // qputenv("QT_OPENGL_BUGLIST", ":/opengl/resources/opengl/buglist.json");
+    qputenv("QT_OPENGL_BUGLIST", ":/opengl/resources/opengl/buglist.json");
 
     // Allow for command line override of renderer
     for (int i = 0; i < argc; i++) {
@@ -145,21 +156,49 @@ int main(int argc, char *argv[])
         if (arg == QStringLiteral("-desktop")) {
             QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
             break;
-        } else if (arg == QStringLiteral("-swrast")) {
+        }
+        else if (arg == QStringLiteral("-swrast")) {
             QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
             break;
         }
     }
-
-// In Windows, the compiler doesn't see the use of the class created by Q_IMPORT_PLUGIN
-#pragma warning( disable : 4930 4101 )
-
 #endif
 
+#ifdef Q_OS_LINUX
+    std::signal(SIGINT, sigHandler);
+    std::signal(SIGTERM, sigHandler);
+#endif /* Q_OS_LINUX */
+
+    // The following calls to qRegisterMetaType are done to silence debug output which warns
+    // that we use these types in signals, and without calling qRegisterMetaType we can't queue
+    // these signals. In general we don't queue these signals, but we do what the warning says
+    // anyway to silence the debug output.
+#ifndef NO_SERIAL_LINK
+    qRegisterMetaType<QSerialPort::SerialPortError>();
+#endif
+#ifdef QGC_ENABLE_BLUETOOTH
+    qRegisterMetaType<QBluetoothSocket::SocketError>();
+    qRegisterMetaType<QBluetoothServiceInfo>();
+#endif
+    qRegisterMetaType<QAbstractSocket::SocketError>();
+#ifndef __mobile__
+#ifndef NO_SERIAL_LINK
+    qRegisterMetaType<QGCSerialPortInfo>();
+#endif
+#endif
+
+    qRegisterMetaType<Vehicle::MavCmdResultFailureCode_t>("Vehicle::MavCmdResultFailureCode_t");
+
     // We statically link our own QtLocation plugin
+
+#ifdef Q_OS_WIN
+    // In Windows, the compiler doesn't see the use of the class created by Q_IMPORT_PLUGIN
+#pragma warning( disable : 4930 4101 )
+#endif
+
     Q_IMPORT_PLUGIN(QGeoServiceProviderFactoryQGC)
 
-    bool runUnitTests = false;          // Run unit tests
+        bool runUnitTests = false;          // Run unit tests
 
 #ifdef QT_DEBUG
     // We parse a small set of command line options here prior to QGCApplication in order to handle the ones
@@ -176,52 +215,97 @@ int main(int argc, char *argv[])
         // Add additional command line option flags here
     };
 
-    ParseCmdLineOptions(argc, argv, rgCmdLineOptions, sizeof(rgCmdLineOptions)/sizeof(rgCmdLineOptions[0]), false);
+    ParseCmdLineOptions(argc, argv, rgCmdLineOptions, sizeof(rgCmdLineOptions) / sizeof(rgCmdLineOptions[0]), false);
     if (stressUnitTests) {
         runUnitTests = true;
     }
 
-#ifdef Q_OS_WIN
     if (quietWindowsAsserts) {
+#ifdef Q_OS_WIN
         _CrtSetReportHook(WindowsCrtReportHook);
+#endif
     }
 
+#ifdef Q_OS_WIN
     if (runUnitTests) {
         // Don't pop up Windows Error Reporting dialog when app crashes. This prevents TeamCity from
         // hanging.
-        const DWORD dwMode = SetErrorMode(SEM_NOGPFAULTERRORBOX);
+        DWORD dwMode = SetErrorMode(SEM_NOGPFAULTERRORBOX);
         SetErrorMode(dwMode | SEM_NOGPFAULTERRORBOX);
     }
-#endif // Q_OS_WIN
+#endif
 #endif // QT_DEBUG
 
-    QGCApplication app(argc, argv, runUnitTests);
+#ifdef Q_OS_DARWIN
+    // Gstreamer video playback requires OpenGL
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+#endif
 
-    #ifdef Q_OS_LINUX
-        std::signal(SIGINT, sigHandler);
-        std::signal(SIGTERM, sigHandler);
-    #endif
+    QQuickStyle::setStyle("Basic");
+    QGCApplication* app = new QGCApplication(argc, argv, runUnitTests);
+    Q_CHECK_PTR(app);
+    if (app->isErrorState()) {
+        app->exec();
+        return -1;
+    }
 
-    app.init();
+#ifdef Q_OS_LINUX
+    QApplication::setWindowIcon(QIcon(":/res/resources/icons/qgroundcontrol.ico"));
+#endif /* Q_OS_LINUX */
+
+    // There appears to be a threading issue in qRegisterMetaType which can cause it to throw a qWarning
+    // about duplicate type converters. This is caused by a race condition in the Qt code. Still working
+    // with them on tracking down the bug. For now we register the type which is giving us problems here
+    // while we only have the main thread. That should prevent it from hitting the race condition later
+    // on in the code.
+    qRegisterMetaType<QList<QPair<QByteArray, QByteArray> > >();
+
+    app->_initCommon();
+    //-- Initialize Cache System
+    getQGCMapEngine()->init();
 
     int exitCode = 0;
 
 #ifdef UNITTEST_BUILD
     if (runUnitTests) {
-        exitCode = runTests(stressUnitTests, unitTestOptions);
-    } else
+        for (int i = 0; i < (stressUnitTests ? 20 : 1); i++) {
+            if (!app->_initForUnitTests()) {
+                return -1;
+            }
+
+            // Run the test
+            int failures = UnitTest::run(unitTestOptions);
+            if (failures == 0) {
+                qDebug() << "ALL TESTS PASSED";
+                exitCode = 0;
+            }
+            else {
+                qDebug() << failures << " TESTS FAILED!";
+                exitCode = -failures;
+                break;
+            }
+        }
+    }
+    else
 #endif
     {
-        #ifdef Q_OS_ANDROID
-            AndroidInterface::checkStoragePermissions();
-        #endif
 
-        exitCode = app.exec();
+#ifdef Q_OS_ANDROID
+        AndroidInterface::checkStoragePermissions();
+        QNativeInterface::QAndroidApplication::hideSplashScreen(333);
+#endif
+        if (!app->_initForNormalAppBoot()) {
+            return -1;
+        }
+        exitCode = app->exec();
     }
 
-    app.shutdown();
+    app->_shutdown();
+    delete app;
+    //-- Shutdown Cache System
+    destroyMapEngine();
 
-    qDebug() << "Exiting main";
+    qDebug() << "After app delete";
 
     return exitCode;
 }
